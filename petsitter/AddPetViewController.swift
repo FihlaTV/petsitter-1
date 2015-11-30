9bcb72e69ddd613fa6ca4dd1e88b1909762523ed
Devin Clark <devin.clark27@gmail.com>
2015-11-30 15:43:42 -0600

    More progress on storyboard

--- petsitter/AddPetViewController.swift
+++ petsitter/AddPetViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class AddPetViewController: UIViewController {
 
+    @IBOutlet weak var personalityLabel: UILabel!
+    @IBOutlet weak var feedingInstructions: UILabel!
+    @IBOutlet weak var activityInstructions: UILabel!
+    @IBOutlet weak var emergencyContact: UILabel!
+    @IBOutlet weak var addPhotoVideo: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setRightBarButtonItem(UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "barButtonItemClicked:"), animated: true)
@@ -20,6 +27,29 @@ class AddPetViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // edit buttons
+    @IBAction func addPersonalityInfoButton(sender: AnyObject) {
+    }
+    
+    @IBAction func addFeedingInstructionsButton(sender: AnyObject) {
+    }
+    
+    @IBAction func addActivityInstructionsButton(sender: AnyObject) {
+    }
+    
+    @IBAction func addEmergencyContactButton(sender: AnyObject) {
+    }
+    
+    @IBAction func addPhotoVideoButton(sender: AnyObject) {
+    }
+    
+    
+    // Segment Buttons
+    @IBAction func segmentChanged(sender: AnyObject) {
+    
+    }
+    
     
     
     /*
